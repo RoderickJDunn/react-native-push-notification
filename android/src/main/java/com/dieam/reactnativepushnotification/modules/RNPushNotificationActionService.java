ffe0d0ba746b4062698fc9f513563a013607dc19
RoderickJDunn <roderickjdunn@gmail.com>
2019-07-02 22:57:44 -0400

    - Made change to Android HeadlessJsTaskService subclass as per react-native documentation @ https://facebook.github.io/react-native/docs/headless-js-android.html ... - This did not fix the issue, but seems to be good practice

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationActionService.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationActionService.java
@@ -1,6 +1,7 @@
 package com.dieam.reactnativepushnotification.modules;
 
 import android.content.Intent;
+import android.os.Bundle;
 
 import com.facebook.react.HeadlessJsTaskService;
 import com.facebook.react.bridge.Arguments;
@@ -9,14 +10,18 @@ import com.facebook.react.jstasks.HeadlessJsTaskConfig;
 import javax.annotation.Nullable;
 
 public class RNPushNotificationActionService extends HeadlessJsTaskService {
-  @Nullable
+
   @Override
-  protected HeadlessJsTaskConfig getTaskConfig(Intent intent) {
-    return new HeadlessJsTaskConfig(
-        "RNPushNotificationActionHandlerTask",
-        Arguments.fromBundle(intent.getExtras()),
-        5000,
-        true
-    );
+  protected @Nullable HeadlessJsTaskConfig getTaskConfig(Intent intent) {
+    Bundle extras = intent.getExtras();
+    if (extras != null) {
+      return new HeadlessJsTaskConfig(
+              "RNPushNotificationActionHandlerTask",
+              Arguments.fromBundle(intent.getExtras()),
+              5000,
+              true
+      );
+    }
+    return null;
   }
 }
